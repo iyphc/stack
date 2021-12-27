dd4142539655d407c7bfdf77b81c53ce77ab3d34
Wohrus <werhistudy@yandex.ru>
2021-12-28 00:34:06 +0500

    added resize function

--- stack.c
+++ stack.c
@@ -6,49 +6,69 @@ const unsigned long long canary = 15000987876788768;
 
 stack* construct() {
   stack* stack = (struct stack*)calloc(1, sizeof(stack));
-  stack->size = 10;
+  stack->size = 4; // не работает при size < 4
   stack->pointer = -1;
-  Exceptions status = OK;
+  stack->status = OK;
   elem_t* arr = (elem_t*)calloc(stack->size+1, sizeof(elem_t));
-  arr[stack->size+1] = (elem_t)canary;
+  arr[stack->pointer+1] = (elem_t)canary;
   (stack->arr) = arr;
   return stack;
 }
 
 void push(stack* stack, elem_t elem) {
   if (verify(stack) != OK) {
+    //stack->arr[stack->pointer+1] = (elem_t)canary;
     return;
   }
-  if (stack->size - 1 > stack->pointer) {
-    stack->pointer++;
-    stack->arr[stack->pointer] = elem;
-  }
-  else {
-    stack->status = size_problem;
-  }
+  stack->pointer++;
+  stack->arr[stack->pointer] = elem;
+  stack->arr[stack->pointer+1] = (elem_t)canary;
+  resize_up(stack);
   if (verify(stack) != OK) {
+    //stack->arr[stack->pointer+1] = (elem_t)canary;
     return;
   }
 }
 
 elem_t pop(stack* stack) {
   if (verify(stack) != OK) {
+    //stack->arr[stack->pointer+1] = (elem_t)canary;
     return (elem_t)-10e7;
   }
   if(stack->pointer >= 0) {
-    return stack->arr[stack->pointer--];
+    elem_t temp = stack->arr[stack->pointer];
+    stack->arr[stack->pointer--] = (elem_t)canary;
+    return temp;
   }
   if (verify(stack) != OK) {
+    //stack->arr[stack->pointer+1] = (elem_t)canary;
     return (elem_t)-10e7;
   }
   return (elem_t)-10e7;
 } 
 
 Exceptions verify(stack* stack) {
-  if(stack->arr[stack->size+1] != (elem_t)canary) {
+  if(stack->arr[stack->pointer+1] != (elem_t)canary) {
     stack->status = size_problem;
     return size_problem;
   }
   stack->status = OK;
   return OK;
+}
+
+
+void resize_up(stack* stack) {
+  if(stack->pointer+1 == stack->size) {
+    stack->arr = realloc(stack->arr, sizeof(elem_t)*(stack->size)*2+1);
+    stack->size*=2;
+  }
+}
+
+void resize_down(stack* stack) {
+  if(stack->size > 4) {
+    stack->arr = realloc(stack->arr, sizeof(elem_t)*((stack->size)/2)+1);
+    stack->size/=2;
+    stack->arr[stack->size] = (elem_t)canary;
+    stack->pointer = stack->size - 1;
+  }
 }
