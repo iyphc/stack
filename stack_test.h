023e9d1a0e3b1cbda6664cdb988c00aca038373b
Wohrus <werhistudy@yandex.ru>
2021-12-28 00:36:37 +0500

    .

---
+++ stack_test.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef enum test_Exceptions {
+  complete,
+  failure  
+} test_Exceptions;
+
+test_Exceptions test_resize_down();
+test_Exceptions test_resize_up();
+test_Exceptions test_construct();
+test_Exceptions test_verify();
+test_Exceptions test_push();
+test_Exceptions test_pop();
+void run_all_tests() ;
