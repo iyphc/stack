e95f6afbb844e85ad5cc4b0188b393e0547b6f12
wohrus <werhistudy@yandex.ru>
2022-02-06 20:33:52 +0300

    .

--- main.c
+++ main.c
@@ -2,10 +2,15 @@
 #include "stack.h"
 #include "stack_test.h"
 
+/**  
+ * @file main.c
+ * @brief Contains main function
+*/
+
 int main() {
  stack* stack = construct();
- for(int i = 0; i < 10; i++) {
-   push(stack, (i+1)*10);
+ for(int i = 0; i < 14; i++) {
+  push(stack, (i+1)*10);
  }
  resize_down(stack);
  for(int i = 0; i < stack->size; i++) {
