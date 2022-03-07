98383df759178406271c384ea8fbd4b6ef71c7ca
wohrus <werhistudy@yandex.ru>
2022-03-07 22:15:41 +0300

    .

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-#include "stack.h"
-//#include "stack_test.h"
+#include "stack_test.h"
 
-/**  
+/**
  * @file main.c
  * @brief Contains main function
-*/
+ */
 
-int main() {
- stack* stack = construct();
- for(int i = 0; i < 14; i++) {
-  push(stack, (i+1)*10);
- }
- printf("%d\n", stack->pointer);
- while(stack->pointer >= 0) {
-  printf("%d\n", (elem_t)pop(stack));
- }
- //run_all_tests();
- return 0; 
+int main()
+{
+  stack *stack = construct();
+  for (int i = 0; i < 14; i++) {
+    push(stack, (i + 1) * 10);
+  }
+  printf("%d\n", stack->pointer);
+  while (stack->pointer >= 0) {
+    printf("%d\n", (elem_t)pop(stack));
+  }
+  run_all_tests();
+  return 0;
 }
